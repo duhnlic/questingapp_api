fdcd6c688ff1b7b2f2d38806873dc0d468487cf2
duhnlic <bpstew22@gmail.com>
2021-04-24 12:21:05 -0500

    Updated .env

--- questapp/questapp/settings.py
+++ questapp/questapp/settings.py
@@ -16,11 +16,14 @@ import environ
 from secrets import *
 from dotenv import load_dotenv
 import os
+# from .questapp.env import *
 
+# questapp = os.path.expanduser('~/questapp')  # adjust as appropriate
+# load_dotenv(os.path.join(questapp, '.env'))
 
 # load_dotenv()
-# env = environ.Env()
-# environ.Env.read_env()
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -30,10 +33,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-u!7l+%y9382f#qxa6i2y6y8$w(ce#=w2m2y83o6ia6^1_*)=3b'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
