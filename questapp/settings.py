406cc040222497733b44de8aaf999d94d11da110
duhnlic <bpstew22@gmail.com>
2021-04-26 16:26:19 -0500

    Updated 'ALLOWED_HOSTS' in settings.py

--- questapp/settings.py
+++ questapp/settings.py
@@ -33,11 +33,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-u!7l+%y9382f#qxa6i2y6y8$w(ce#=w2m2y83o6ia6^1_*)=3b'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
