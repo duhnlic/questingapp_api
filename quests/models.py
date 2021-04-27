f45863161520295b004127e39b67ed94a659fd26
duhnlic <bpstew22@gmail.com>
2021-04-26 22:16:49 -0500

    Updated Production API

--- quests/models.py
+++ quests/models.py
@@ -3,17 +3,14 @@ from django.db import models
 
 class User(models.Model):
     username = models.CharField(max_length=10, default="username")
-    # first_name = models.CharField(max_length=50, default="first name")
-    # last_name = models.CharField(max_length=50, default="last name")
 
     def __str__(self):
         return self.username
 
 class Quest(models.Model):
-    name = models.CharField(max_length=100, default="name")
+    name = models.ForeignKey(User, on_delete=models.CASCADE, related_name="quest")
     stat = models.CharField(max_length=100, default="0")
     total = models.CharField(max_length=100, default="0")
-    active_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="quest", null=True, blank=True)
 
     def __str__(self):
         return self.name
